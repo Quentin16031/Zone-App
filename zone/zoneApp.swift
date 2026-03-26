bf2b10d84abaeb19b68cd434f9079f396ea887d3
Quentin <quentin16031@gmail.com>
2026-03-26 14:40:47 +0100

    Initial Commit

---
+++ zone/zoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  zoneApp.swift
+//  zone
+//
+//  Created by Quentin REMI on 26/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct zoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
